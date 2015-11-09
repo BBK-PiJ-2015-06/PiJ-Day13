63e757cd44df9577fc854c2c2403f6721b0391d0
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-09 15:02:51 +0000

    New test file TestUser created to test methods within User.

---
+++ TestUser.java
@@ -0,0 +1,18 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TestUser {
+	
+	private User newUser;
+	
+	@Before
+	public void setUp() {
+		newUser = new UserImpl("Name");
+	}
+	
+	@Test
+	public void testsRetrievalOfUserName() {
+		assertEquals("Name", newUser.getName());
+	}
+	
+}
