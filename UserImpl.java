40dad89512059a6352451d8f33edf3c182ee7a97
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-09 16:19:41 +0000

    Minimum amount of code added to UserImpl to enable TestUser tests for section 1.3 to pass.

--- UserImpl.java
+++ UserImpl.java
@@ -2,6 +2,7 @@ public class UserImpl implements User {
 	
 	private String name;
 	private int libraryID;
+	private String libraryName;
 	
 	public UserImpl(String name) {
 		this.name = name;
@@ -13,6 +14,15 @@ public class UserImpl implements User {
 	
 	public int getLibraryId() {
 		return this.libraryID;
+	}
+	
+	public void register(Library library) {
+		this.libraryName = library.getName();
+		this.libraryID = library.getId();
+	}
+	
+	public String getLibrary() {
+		return this.libraryName;
 	}
 	
 }
