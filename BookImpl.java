fbe76be46ce1e9e4edc95806b42a71727335ee8e
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-09 14:21:06 +0000

    New class BookImpl created with basic structure to ensure TestBook class compiles. Tests result in 2 failures.

---
+++ BookImpl.java
@@ -0,0 +1,19 @@
+public class BookImpl implements Book {
+	
+	private String title;
+	private String author;
+	
+	public BookImpl(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+	
+	public String getTitle() {
+		return null;
+	}
+	
+	public String getAuthor() {
+		return null;
+	}
+	
+} 
