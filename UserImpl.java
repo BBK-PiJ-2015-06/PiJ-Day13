d99ddc2b7751621cd4c688ab260a6783a3e9b843
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-09 15:11:28 +0000

    New class called UserImpl created that implements User.

---
+++ UserImpl.java
@@ -0,0 +1,18 @@
+public class UserImpl implements User {
+	
+	private String name;
+	private int libraryID;
+	
+	public UserImpl(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return null;
+	}
+	
+	public int getLibraryId() {
+		return 0;
+	}
+	
+}
