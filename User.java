e69733b6eae8abd6dfd3064c3e59105b346e1428
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-11-10 21:10:16 +0000

    Methods added to enable user to borrow, return and store borrowed books.

--- User.java
+++ User.java
@@ -4,6 +4,9 @@
   * library ID is not. 
   *
   * Both the name and library ID of the user must be unique in a library.
+  *
+  * A User is also able to borrow Books from a Library's collection once a 
+  * User has registered with the Library. 
   */
 public interface User {
 	
@@ -35,4 +38,35 @@ public interface User {
 	 * @return the name of the library that this User is a member of
 	 */
 	String getLibrary();
+	
+	/**
+	 * Enables the User to borrow a Book from the Library's collection that
+	 * the user is registered with.
+	 *
+	 * If the User is not registered with a Library, this method performs no 
+	 * functionality. If the title of the Book is not found within the Library
+	 * collection, the method performs no functionality.
+	 *
+	 * @param title the title of the Book that the User will borrow
+	 */
+	void borrowBook(String title);
+	
+	/**
+	 * Enables the User to return a Book to the Library's collection that
+	 * the user is registered with.
+	 *
+	 * If the User is not registered with a Library, this method performs no 
+	 * functionality. If the title of the Book is not found within the User's
+	 * collection, the method performs no functionality.
+	 *
+	 * @param title the title of the Book that the User will return
+	 */
+	void returnBook(String title);
+	
+	/**
+	 * Returns a list of the Books that this User is currently borrowing.
+	 *
+	 * @return an array list of Books that the User is currently borrowing
+	 */
+	Book[] getBorrowedBooks();
 }
--- UserImpl.java
+++ UserImpl.java
@@ -3,9 +3,12 @@ public class UserImpl implements User {
 	private String name;
 	private int libraryID;
 	private String libraryName;
+	private Book[] borrowedBookList;
+	private Library lib;
 	
 	public UserImpl(String name) {
 		this.name = name;
+		this.borrowedBookList = null;
 	}
 	
 	public String getName() {
@@ -18,11 +21,46 @@ public class UserImpl implements User {
 	
 	public void register(Library library) {
 		this.libraryName = library.getName();
+		this.lib = library;
 		this.libraryID = library.getId(this.name);
+		this.borrowedBookList = new Book[0];
 	}
 	
 	public String getLibrary() {
 		return this.libraryName;
 	}
 	
+	public void borrowBook(String title) {
+		if(this.borrowedBookList != null) {
+			Book[] temp = new Book[this.borrowedBookList.length + 1];
+			for(int index = 0; index < this.borrowedBookList.length; index++) {
+				temp[index] = this.borrowedBookList[index];
+			}
+			temp[temp.length - 1] = this.lib.takeBook(title);
+			borrowedBookList = temp;
+		}
+	}
+
+	public void returnBook(String title) {
+		int index = -1;
+		for(int i = 0; i < this.borrowedBookList.length; i++) {
+			if(this.borrowedBookList[i].getTitle().equals(title)) {
+				index = i;
+			}
+		}
+		if(index >= 0) {
+			this.lib.returnBook(this.borrowedBookList[index]);
+			Book[] temp = new Book[this.borrowedBookList.length - 1];
+			for(int i = 0; i < temp.length; i++) {
+				if(i != index) {
+					temp[i] = this.borrowedBookList[i];
+				}
+			}
+			borrowedBookList = temp;
+		}
+	}
+
+	public Book[] getBorrowedBooks() {
+		return this.borrowedBookList;
+	}
 }
